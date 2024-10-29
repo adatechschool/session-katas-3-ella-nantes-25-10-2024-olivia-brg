d27b951dcff30c89ecc796fba03cdd34cf2ee81e
Olivia <olivia.bergaglia@gmail.com>
2024-10-29 12:24:07 +0100

    first commit

---
+++ src/main/java/com/pokedex/Main.java
@@ -0,0 +1,13 @@
+package com.pokedex;
+
+// Main.java
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
